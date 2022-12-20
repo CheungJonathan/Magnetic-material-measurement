b1f3c5f72e71c02141e3b5bff15e5db35fa0ee26
CheungJonathan <skcheungag@connect.ust.hk>
2022-12-21 01:51:40 +0800

    keep pyvisa

--- equipment/pg1072.py
+++ equipment/pg1072.py
@@ -1,16 +1,16 @@
-import vxi11 as py
+import pyvisa as py
 
 # address_pg1072 = 'TCPIP0::AT::inst0::INSTR' # enter the keithley 2182a address here
 
 class pg1072:
     
     def __init__(self,address_pg1072):
-
-        self.equ_pg1072= vxi11.Instrument(address_pg1072)
+        rm = py.ResourceManager()
+        self.equ_pg1072= rm.open_resource(address_pg1072)
 
     def info(self):
         # return the information of the equipment
-        return self.equ_pg1072.ask("*IDN?")
+        return self.equ_pg1072.query("*IDN?")
 
     def reset(self):
         self.equ_pg1072.write("*RST")
