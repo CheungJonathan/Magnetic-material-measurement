8b0c974260f29c6e3e9a5d60e14b3f9a10db7c9b
CheungJonathan <skcheungag@connect.ust.hk>
2022-12-21 01:47:32 +0800

    improve pg1072

--- equipment/pg1072.py
+++ equipment/pg1072.py
@@ -1,16 +1,16 @@
-import pyvisa as py
+import vxi11 as py
 
 # address_pg1072 = 'TCPIP0::AT::inst0::INSTR' # enter the keithley 2182a address here
 
 class pg1072:
     
     def __init__(self,address_pg1072):
-        rm = py.ResourceManager()
-        self.equ_pg1072= rm.open_resource(address_pg1072)
+
+        self.equ_pg1072= vxi11.Instrument(address_pg1072)
 
     def info(self):
         # return the information of the equipment
-        return self.equ_pg1072.query("*IDN?")
+        return self.equ_pg1072.ask("*IDN?")
 
     def reset(self):
         self.equ_pg1072.write("*RST")
@@ -36,12 +36,12 @@ class pg1072:
         self.equ_pg1072.write("*TRG")
 
     def set_pulse_period(self,period,channel=1,unit_period='ns'):
-        self.equ_pg1072.write(f"SOURce{channel},PERiod {period} {unit_period}")
+        self.equ_pg1072.write(f"SOURce{channel}:PERiod {period} {unit_period}")
     
     def set_pulse_vol(self,vol_high,vol_low,channel=1):
-        self.equ_pg1072.write(f"SOURce{channel},VOLT HIGH {vol_high}")
-        self.equ_pg1072.write(f"SOURce{channel},VOLT LOW {vol_low}")
+        self.equ_pg1072.write(f"SOURce{channel}:VOLT HIGH {vol_high}")
+        self.equ_pg1072.write(f"SOURce{channel}:VOLT LOW {vol_low}")
 
     def set_pulse_width(self,width,channel=1,unit_period='ns'):
-        self.equ_pg1072.write(f"SOURce{channel},WIDth {width} {unit_period}")
+        self.equ_pg1072.write(f"SOURce{channel}:WIDth {width} {unit_period}")
     
